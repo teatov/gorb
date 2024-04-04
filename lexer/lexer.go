d4f3e476a6a5b41845486b836ef874b7d3b35558
teatov <hlebpoimenigleb@gmail.com>
2024-04-04 21:58:53 +1000

    reorganize lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,6 +2,12 @@ package lexer
 
 import "gorb/token"
 
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
 type Lexer struct {
 	input        string
 	position     int
@@ -17,47 +23,6 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
-}
-
-func isWhitespace(ch byte) bool {
-	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
-}
-
-func (l *Lexer) skipWhitespace() {
-	for isWhitespace(l.ch) {
-		l.readChar()
-	}
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-func isLetter(ch byte) bool {
-	isLowercaseLetter := 'a' <= ch && ch <= 'z'
-	isUppercaseLetter := 'A' <= ch && ch <= 'Z'
-	isUnderscore := ch == '_'
-	return isLowercaseLetter || isUppercaseLetter || isUnderscore
-}
-
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
-}
-
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
 }
 
 func (l *Lexer) NextToken() token.Token {
@@ -102,8 +67,43 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
+func isWhitespace(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for isWhitespace(l.ch) {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	isLowercaseLetter := 'a' <= ch && ch <= 'z'
+	isUppercaseLetter := 'A' <= ch && ch <= 'Z'
+	isUnderscore := ch == '_'
+	return isLowercaseLetter || isUppercaseLetter || isUnderscore
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
