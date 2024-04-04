ae58955536a4c70121c4244b069dcd7ca00f1e3d
teatov <hlebpoimenigleb@gmail.com>
2024-04-04 20:54:38 +1000

    add first token types

--- token/token.go
+++ token/token.go
@@ -2,6 +2,27 @@ package token
 
 type TokenType string
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// identifiers and literals
+	IDENTIFIER = "IDENTIFIER"
+	INT        = "INT"
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	PAREN_L   = "("
+	PAREN_R   = ")"
+	BRACE_L   = "{"
+	BRACE_R   = "}"
+	//keywords
+	FUNCTION    = "FUNCTION"
+	DECLARATION = "DECLARATION"
+)
+
 type Token struct {
 	Type    TokenType
 	Literal string
