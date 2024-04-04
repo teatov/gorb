ce3d75a14b160c680f20e3707b31b0ba20c20485
teatov <hlebpoimenigleb@gmail.com>
2024-04-04 21:12:09 +1000

    add readChar method to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,7 +7,18 @@ type Lexer struct {
 	ch           byte
 }
 
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
