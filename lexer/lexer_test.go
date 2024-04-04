084fd9e49ade76d37fca2f5000774d520a771b7b
teatov <hlebpoimenigleb@gmail.com>
2024-04-04 21:31:02 +1000

    change lexer test case to actual syntax

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,19 +6,53 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `so five = 5;
+	so ten = 10;
+	
+	so add = fn(x, y) {
+		x + y;
+	}
+	
+	so result = add(five, ten);`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.DECLARATION, "so"},
+		{token.IDENTIFIER, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.DECLARATION, "so"},
+		{token.IDENTIFIER, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.DECLARATION, "so"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.PAREN_L, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.PAREN_R, ")"},
 		{token.BRACE_L, "{"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
+		{token.SEMICOLON, ";"},
 		{token.BRACE_R, "}"},
+		{token.SEMICOLON, ";"},
+		{token.DECLARATION, "so"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.PAREN_L, "("},
+		{token.IDENTIFIER, "five"},
 		{token.COMMA, ","},
+		{token.IDENTIFIER, "ten"},
+		{token.PAREN_R, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
